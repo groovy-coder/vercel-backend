7c1cdc2722b350f66c692ee03be38599dfa7d340
kaustav <155538678+groovy-coder@users.noreply.github.com>
2024-04-04 21:50:44 +0530

    Delete vercel directory

--- vercel/src/utils.ts
+++
@@ -1,9 +0,0 @@
-const MAX_LEN=5;
-export function generate(){
-    let ans="";
-    const subset="1234567890hyagszdchjalsdm";
-    for(let i=0;i<MAX_LEN;i++){
-        ans+=subset[Math.floor(Math.random()*subset.length)];
-    }
-    return ans;
-}
